8534ba355aa08d3ec8c8979feb8990b6f8b1c7b8
Andres Calderon <andresfcaalderon@gmail.com>
2021-05-17 18:30:37 -0500

    Ajustes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('./dist/pokedex'));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/pokedex/index.html'));
+  res.sendFile(path.join('dist/pokedex/index.html'));
 });
 app.listen(process.env.PORT || 8090);
 console.log('server listening on port 8090')
