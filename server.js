810c0f54217dddb455c429d2eff7f1b371b7a98a
Andres Calderon <andresfcaalderon@gmail.com>
2021-05-17 18:12:48 -0500

    Ajustes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
 const app = express();
-app.use(express.static('./dist/pokedex'));
+app.use(express.static('./src'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/pokedex/'}
+  res.sendFile('index.html', {root: 'src/'}
 );
 });
 app.listen(process.env.PORT || 8090);
